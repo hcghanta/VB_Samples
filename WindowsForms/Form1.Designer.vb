e1829d7e3f0fb8b9ddb6a30cac6c4609d63a9447
Hemachandra <hemachandra.ghanta@yash.com>
2020-02-04 15:40:51 -0600

    simple csv to grid and export to csv

---
+++ WindowsForms/Form1.Designer.vb
@@ -0,0 +1,210 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column6 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column7 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column8 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column9 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column10 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.PathTextBox = New System.Windows.Forms.TextBox()
+        Me.ExportButton = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 50)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(112, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Select File"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5, Me.Column6, Me.Column7, Me.Column8, Me.Column9, Me.Column10})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 90)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersWidth = 51
+        Me.DataGridView1.RowTemplate.Height = 24
+        Me.DataGridView1.Size = New System.Drawing.Size(1303, 479)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Column1
+        '
+        DataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle1.Format = "N0"
+        DataGridViewCellStyle1.NullValue = Nothing
+        Me.Column1.DefaultCellStyle = DataGridViewCellStyle1
+        Me.Column1.HeaderText = "Id"
+        Me.Column1.MinimumWidth = 6
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 125
+        '
+        'Column2
+        '
+        DataGridViewCellStyle2.NullValue = Nothing
+        Me.Column2.DefaultCellStyle = DataGridViewCellStyle2
+        Me.Column2.HeaderText = "Item"
+        Me.Column2.MinimumWidth = 6
+        Me.Column2.Name = "Column2"
+        Me.Column2.Width = 125
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Owner"
+        Me.Column3.MinimumWidth = 6
+        Me.Column3.Name = "Column3"
+        Me.Column3.Width = 125
+        '
+        'Column4
+        '
+        DataGridViewCellStyle3.Format = "N0"
+        DataGridViewCellStyle3.NullValue = Nothing
+        Me.Column4.DefaultCellStyle = DataGridViewCellStyle3
+        Me.Column4.HeaderText = "Quantity"
+        Me.Column4.MinimumWidth = 6
+        Me.Column4.Name = "Column4"
+        Me.Column4.Width = 125
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Unknown"
+        Me.Column5.MinimumWidth = 6
+        Me.Column5.Name = "Column5"
+        Me.Column5.Width = 125
+        '
+        'Column6
+        '
+        DataGridViewCellStyle4.Format = "N2"
+        DataGridViewCellStyle4.NullValue = Nothing
+        Me.Column6.DefaultCellStyle = DataGridViewCellStyle4
+        Me.Column6.HeaderText = "SellingPrice"
+        Me.Column6.MinimumWidth = 6
+        Me.Column6.Name = "Column6"
+        Me.Column6.Width = 125
+        '
+        'Column7
+        '
+        DataGridViewCellStyle5.Format = "N2"
+        DataGridViewCellStyle5.NullValue = Nothing
+        Me.Column7.DefaultCellStyle = DataGridViewCellStyle5
+        Me.Column7.HeaderText = "MSRP"
+        Me.Column7.MinimumWidth = 6
+        Me.Column7.Name = "Column7"
+        Me.Column7.Width = 125
+        '
+        'Column8
+        '
+        Me.Column8.HeaderText = "Manufacturer"
+        Me.Column8.MinimumWidth = 6
+        Me.Column8.Name = "Column8"
+        Me.Column8.Width = 125
+        '
+        'Column9
+        '
+        Me.Column9.HeaderText = "Category"
+        Me.Column9.MinimumWidth = 6
+        Me.Column9.Name = "Column9"
+        Me.Column9.Width = 125
+        '
+        'Column10
+        '
+        DataGridViewCellStyle6.Format = "N2"
+        DataGridViewCellStyle6.NullValue = Nothing
+        Me.Column10.DefaultCellStyle = DataGridViewCellStyle6
+        Me.Column10.HeaderText = "Discount"
+        Me.Column10.MinimumWidth = 6
+        Me.Column10.Name = "Column10"
+        Me.Column10.Width = 125
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'PathTextBox
+        '
+        Me.PathTextBox.Location = New System.Drawing.Point(13, 22)
+        Me.PathTextBox.Name = "PathTextBox"
+        Me.PathTextBox.Size = New System.Drawing.Size(311, 22)
+        Me.PathTextBox.TabIndex = 2
+        '
+        'ExportButton
+        '
+        Me.ExportButton.Location = New System.Drawing.Point(171, 50)
+        Me.ExportButton.Name = "ExportButton"
+        Me.ExportButton.Size = New System.Drawing.Size(105, 23)
+        Me.ExportButton.TabIndex = 3
+        Me.ExportButton.Text = "Export to csv"
+        Me.ExportButton.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1461, 596)
+        Me.Controls.Add(Me.ExportButton)
+        Me.Controls.Add(Me.PathTextBox)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "CSV Loader"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents Column6 As DataGridViewTextBoxColumn
+    Friend WithEvents Column7 As DataGridViewTextBoxColumn
+    Friend WithEvents Column8 As DataGridViewTextBoxColumn
+    Friend WithEvents Column9 As DataGridViewTextBoxColumn
+    Friend WithEvents Column10 As DataGridViewTextBoxColumn
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents PathTextBox As TextBox
+    Friend WithEvents ExportButton As Button
+End Class
---
+++ WindowsForms/Form1.vb
@@ -0,0 +1,94 @@
+﻿Imports System.IO
+Imports System.Text
+Imports FileHelpers
+
+Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        openFileDialog1.Title = "Please select a DB file"
+        OpenFileDialog1.InitialDirectory = "D:\Visual Basic\"
+        OpenFileDialog1.Filter = "CSV Files|*.csv"
+        Dim fpath As String = ""
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            fpath = OpenFileDialog1.FileName
+        End If
+
+        Dim EngOrders As FileHelperEngine(Of Orders) = New FileHelperEngine(Of Orders)
+        Dim res = EngOrders.ReadFile(fpath)
+
+        PathTextBox.Text = fpath
+        Dim fname As String = fpath
+        Dim threader As New StreamReader(fname, Encoding.Default)
+        Dim sline As String = ""
+
+        Do
+            sline = threader.ReadLine
+            If sline Is Nothing Then Exit Do
+            If sline.Contains("""") Then
+                Dim fquote As Integer = sline.IndexOf("""")
+                Dim lquote As Integer = sline.LastIndexOf("""")
+                Dim slineLength As Integer = sline.Length
+
+                Dim sub1 As String = sline.Substring(0, fquote)
+                Dim sub2 As String = sline.Substring(fquote, lquote - 2).Replace(",", "")
+                Dim sub3 As String = sline.Substring(lquote, slineLength - lquote)
+
+                sline = sub1 + sub2 + sub3
+            End If
+
+            Dim words() As String = sline.Split(",")
+            Dim value As String = ""
+            DataGridView1.Rows.Add("")
+
+            For ix As Integer = 0 To 9
+                Dim currentrow As String = DataGridView1.Rows.Count - 1
+                DataGridView1.Rows(currentrow).Cells(ix).Value = words(ix)
+            Next
+
+            If Not IsNumeric(words(0)) Then DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(0).Style.BackColor = Color.Yellow
+        Loop
+        DataGridView1.Rows.Add("")
+        threader.Close()
+
+    End Sub
+
+    Private Sub ExportButton_Click(sender As Object, e As EventArgs) Handles ExportButton.Click
+        Dim StrExport As String = ""
+        For Each C As DataGridViewColumn In DataGridView1.Columns
+            StrExport &= """" & C.HeaderText & ""","
+        Next
+        StrExport = StrExport.Substring(0, StrExport.Length - 1)
+        StrExport &= Environment.NewLine
+
+        For Each R As DataGridViewRow In DataGridView1.Rows
+            For Each C As DataGridViewCell In R.Cells
+                If Not C.Value Is Nothing Then
+                    StrExport &= """" & C.Value.ToString & ""","
+                Else
+                    StrExport &= """" & "" & ""","
+                End If
+            Next
+            StrExport = StrExport.Substring(0, StrExport.Length - 1)
+            StrExport &= Environment.NewLine
+        Next
+
+        Dim tw As IO.TextWriter = New IO.StreamWriter("D:\Visual Basic\exportfile.csv")
+        tw.Write(StrExport)
+        tw.Close()
+        MessageBox.Show("File Saved in ""D: \Visual Basic\exportfile.csv""")
+    End Sub
+End Class
+
+<DelimitedRecord(",")>
+Public Class Orders
+    Public Property Id As Integer
+    Public Property Item As String
+    Public Property Owner As String
+    Public Property Quantity As Integer
+    Public Property Unknown As Double
+    Public Property SellingPrice As Double
+    Public Property MSRP As Double
+    Public Property Manufacturer As String
+    Public Property Category As String
+    Public Property Discount As Double
+End Class
