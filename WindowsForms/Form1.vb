87a2b7722d9c19fe41dcee95c703e4e5d186f16f
Hemachandra <hemachandra.ghanta@yash.com>
2020-02-06 10:12:19 -0600

    using filehelpers and microsoft excel library

--- WindowsForms/Form1.vb
+++ WindowsForms/Form1.vb
@@ -1,11 +1,13 @@
 ﻿Imports System.IO
 Imports System.Text
 Imports FileHelpers
+Imports Microsoft.Office.Interop.Excel
 
 Public Class Form1
 
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        openFileDialog1.Title = "Please select a DB file"
+        OpenFileDialog1.Title = "Please select a DB file"
         OpenFileDialog1.InitialDirectory = "D:\Visual Basic\"
         OpenFileDialog1.Filter = "CSV Files|*.csv"
         Dim fpath As String = ""
@@ -15,69 +17,62 @@ Public Class Form1
 
         Dim EngOrders As FileHelperEngine(Of Orders) = New FileHelperEngine(Of Orders)
         Dim res = EngOrders.ReadFile(fpath)
-
+        'Bind res to DataGridView1
         PathTextBox.Text = fpath
-        Dim fname As String = fpath
-        Dim threader As New StreamReader(fname, Encoding.Default)
-        Dim sline As String = ""
-
-        Do
-            sline = threader.ReadLine
-            If sline Is Nothing Then Exit Do
-            If sline.Contains("""") Then
-                Dim fquote As Integer = sline.IndexOf("""")
-                Dim lquote As Integer = sline.LastIndexOf("""")
-                Dim slineLength As Integer = sline.Length
-
-                Dim sub1 As String = sline.Substring(0, fquote)
-                Dim sub2 As String = sline.Substring(fquote, lquote - 2).Replace(",", "")
-                Dim sub3 As String = sline.Substring(lquote, slineLength - lquote)
-
-                sline = sub1 + sub2 + sub3
-            End If
-
-            Dim words() As String = sline.Split(",")
-            Dim value As String = ""
-            DataGridView1.Rows.Add("")
-
-            For ix As Integer = 0 To 9
-                Dim currentrow As String = DataGridView1.Rows.Count - 1
-                DataGridView1.Rows(currentrow).Cells(ix).Value = words(ix)
-            Next
-
-            If Not IsNumeric(words(0)) Then DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(0).Style.BackColor = Color.Yellow
-        Loop
-        DataGridView1.Rows.Add("")
-        threader.Close()
+        'using data table
+        For Each item In res
+            DataGridView1.Rows.Add(item.Id, item.Item, item.Owner, item.Quantity, item.Unknown, item.SellingPrice, item.MSRP, item.Manufacturer, item.Category, item.Discount)
+        Next
 
     End Sub
 
     Private Sub ExportButton_Click(sender As Object, e As EventArgs) Handles ExportButton.Click
-        Dim StrExport As String = ""
-        For Each C As DataGridViewColumn In DataGridView1.Columns
-            StrExport &= """" & C.HeaderText & ""","
-        Next
-        StrExport = StrExport.Substring(0, StrExport.Length - 1)
-        StrExport &= Environment.NewLine
+        Dim xlApp As Microsoft.Office.Interop.Excel.Application
+        Dim xlWorkBook As Microsoft.Office.Interop.Excel.Workbook
+        Dim xlWorkSheet As Microsoft.Office.Interop.Excel.Worksheet
+        Dim misValue As Object = System.Reflection.Missing.Value
+        Dim i As Integer
+        Dim j As Integer
 
-        For Each R As DataGridViewRow In DataGridView1.Rows
-            For Each C As DataGridViewCell In R.Cells
-                If Not C.Value Is Nothing Then
-                    StrExport &= """" & C.Value.ToString & ""","
-                Else
-                    StrExport &= """" & "" & ""","
-                End If
+        xlApp = New Application
+        xlWorkBook = xlApp.Workbooks.Add(misValue)
+        xlWorkSheet = xlWorkBook.Sheets("sheet1")
+
+
+        For i = 0 To DataGridView1.RowCount - 1
+            For j = 0 To DataGridView1.ColumnCount - 1
+                For k As Integer = 1 To DataGridView1.Columns.Count
+                    xlWorkSheet.Cells(1, k) = DataGridView1.Columns(k - 1).HeaderText
+                    xlWorkSheet.Cells(i + 2, j + 1) = DataGridView1(j, i).Value.ToString()
+                Next
             Next
-            StrExport = StrExport.Substring(0, StrExport.Length - 1)
-            StrExport &= Environment.NewLine
         Next
 
-        Dim tw As IO.TextWriter = New IO.StreamWriter("D:\Visual Basic\exportfile.csv")
-        tw.Write(StrExport)
-        tw.Close()
-        MessageBox.Show("File Saved in ""D: \Visual Basic\exportfile.csv""")
+        xlWorkSheet.SaveAs("D:\Visual Basic\Exported Data\vbexcel.xlsx")
+        xlWorkBook.Close()
+        xlApp.Quit()
+
+        releaseObject(xlApp)
+        releaseObject(xlWorkBook)
+        releaseObject(xlWorkSheet)
+
+        MsgBox("You can find the file D:\vbexcel.xlsx")
     End Sub
+
+    Private Sub releaseObject(ByVal obj As Object)
+        Try
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(obj)
+            obj = Nothing
+        Catch ex As Exception
+            obj = Nothing
+        Finally
+            GC.Collect()
+        End Try
+    End Sub
+
 End Class
+
+
 
 <DelimitedRecord(",")>
 Public Class Orders
